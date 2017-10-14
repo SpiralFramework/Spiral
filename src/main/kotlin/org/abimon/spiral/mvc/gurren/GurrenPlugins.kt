5881eb0fbeda2d4e8213f118d7fbd08f95c47636
Isaac <undermybrella@abimon.org>
2017-10-14 20:26:22 +1100

    Format newlines a bit better for readability

--- src/main/kotlin/org/abimon/spiral/mvc/gurren/GurrenPlugins.kt
+++ src/main/kotlin/org/abimon/spiral/mvc/gurren/GurrenPlugins.kt
@@ -30,27 +30,34 @@ object GurrenPlugins {
 
             when(signed) {
                 EnumSignedStatus.UNSIGNED -> {
+                    println()
                     println("**WARNING**")
                     println("${config.name} v${config.version} (${file.name}) is an **unsigned** plugin.")
                     println("This means that it hasn't been officially verified, and may therefore be malicious")
                     println("Only continue to enable this plugin if you trust the plugin developer and the download source.")
+                    println()
+
                     if(!question("Enable ${config.name} (Y/n)? ", "Y"))
                         return@Command
                 }
                 EnumSignedStatus.INVALID_SIGNATURE -> {
+                    println()
                     println("**ERROR**")
                     println("${config.name} v${config.version} (${file.name}) has an **invalid** signature.")
                     println("This means that, while the plugin and version are officially verified, the file you have downloaded does not match the provided signature")
                     println("This should only happen if the plugin author has misconfigured their plugin, or if the file you have downloaded is not the plugin it claims to be")
                     println("This plugin therefore cannot be enabled. Please contact the plugin author to report this error.")
+                    println()
 
                     return@Command
                 }
                 EnumSignedStatus.NO_PUBLIC_KEY -> {
+                    println()
                     println("**ERROR**")
                     println("SPIRAL could not find it's public key from the mod repository.")
                     println("Verifying signatures is therefore impossible, and absolute caution should be taken.")
                     println("Please report this to a SPIRAL developer as soon as possible, and only proceed to enable this plugin if you trust the plugin developer and the download source")
+                    println()
 
                     if(!question("Enable ${config.name} (Y/n)? ", "Y"))
                         return@Command
