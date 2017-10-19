d0e7f6a27e57c66e28ddce1f9e11be195e505b54
Isaac <undermybrella@abimon.org>
2017-10-20 01:13:39 +1100

    Fix plugin searching

--- src/main/kotlin/org/abimon/spiral/modding/data/SpiralModData.kt
+++ src/main/kotlin/org/abimon/spiral/modding/data/SpiralModData.kt
@@ -2,7 +2,7 @@ package org.abimon.spiral.modding.data
 
 data class SpiralModData(
         val uid: String,
-        val is_plugin: Boolean,
+        val plugin: Boolean,
         val latest_version: String,
         val mod_name: String,
         val short_desc: String?
