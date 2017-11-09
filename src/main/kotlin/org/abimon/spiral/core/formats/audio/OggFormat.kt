803d36de6dd151bab8a7bcf0ae2d4b4fe03307c8
Isaac <undermybrella@abimon.org>
2017-11-09 14:04:41 +1100

    Don't spew errors out for SPC files, and fix the header for Ogg files

--- src/main/kotlin/org/abimon/spiral/core/formats/audio/OggFormat.kt
+++ src/main/kotlin/org/abimon/spiral/core/formats/audio/OggFormat.kt
@@ -7,7 +7,7 @@ object OggFormat: SpiralFormat {
     override val name: String = "Ogg"
     override val extension: String? = "ogg"
     override val conversions: Array<SpiralFormat> = emptyArray()
-    val header = byteArrayOf(0x4F, 0x67, 0x67, 0x53, 0x00, 0x02)
+    val header = byteArrayOf(0x4F, 0x67, 0x67, 0x53, 0x00, 0x02, 0x00, 0x00)
 
     override fun isFormat(source: DataSource): Boolean = source.use { stream -> ByteArray(8).apply { stream.read(this) } contentEquals header }
 }
