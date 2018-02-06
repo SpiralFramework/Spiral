ea001f50e88e3875d0099930a833f40cc39b2abc
Isaac <undermybrella@abimon.org>
2018-02-06 20:29:28 +1100

    Remove random comment from DR1.kt and add DR2

--- src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/DR2.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/DR2.kt
@@ -2,9 +2,9 @@ package org.abimon.spiral.core.objects.game.hpa
 
 import org.abimon.spiral.core.utils.DataMapper
 
-object DR1 : HopesPeakDRGame {
+object DR2 : HopesPeakDRGame {
     override val pakNames: Map<String, Array<String>> =
-            DataMapper.readMapFromStream(DR1::class.java.classLoader.getResourceAsStream("pak/dr1.json"))?.mapValues { (_, value) ->
+            DataMapper.readMapFromStream(DR1::class.java.classLoader.getResourceAsStream("pak/dr2.json"))?.mapValues { (_, value) ->
                 ((value as? List<*>)?.asIterable() ?: (value as? Array<*>)?.asIterable())?.mapNotNull { str -> str as? String }?.toTypedArray() ?: emptyArray()
             } ?: emptyMap()
 }
