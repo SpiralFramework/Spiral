0b6240b4ef8fd05bbeab4387b22843794f7ba501
Isaac <undermybrella@abimon.org>
2018-03-18 00:55:36 +1100

    Add wad compilation support (No optimisations mind you)

--- src/main/kotlin/org/abimon/spiral/core/archives/WADArchive.kt
+++ src/main/kotlin/org/abimon/spiral/core/archives/WADArchive.kt
@@ -1,18 +1,18 @@
 package org.abimon.spiral.core.archives
 
+import org.abimon.spiral.core.data.CacheHandler
 import org.abimon.spiral.core.data.SpiralData
 import org.abimon.spiral.core.formats.SpiralFormat
 import org.abimon.spiral.core.formats.images.*
 import org.abimon.spiral.core.objects.UnsafeWAD
 import org.abimon.spiral.core.objects.archives.WAD
+import org.abimon.spiral.core.objects.customWAD
 import org.abimon.spiral.core.utils.DelegatedInputStream
 import org.abimon.spiral.core.utils.WindowedInputStream
 import org.abimon.spiral.modding.BackupManager
 import org.abimon.spiral.modding.data.ModList
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.InputStream
+import java.io.*
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 class WADArchive(override val archiveFile: File) : IArchive {
@@ -36,7 +36,7 @@ class WADArchive(override val archiveFile: File) : IArchive {
                 override fun read(): Int {
                     totalReadOperations.incrementAndGet()
                     val read = super.read()
-                    if(read != -1)
+                    if (read != -1)
                         totalReadBytes.incrementAndGet()
                     return read
                 }
@@ -44,7 +44,7 @@ class WADArchive(override val archiveFile: File) : IArchive {
                 override fun read(b: ByteArray): Int {
                     val readBytes = super.read(b)
                     totalReadOperations.incrementAndGet()
-                    if(readBytes != -1)
+                    if (readBytes != -1)
                         totalReadBytes.addAndGet(readBytes)
                     return readBytes
                 }
@@ -52,7 +52,7 @@ class WADArchive(override val archiveFile: File) : IArchive {
                 override fun read(b: ByteArray, off: Int, len: Int): Int {
                     val readBytes = super.read(b, off, len)
                     totalReadOperations.incrementAndGet()
-                    if(readBytes != -1)
+                    if (readBytes != -1)
                         totalReadBytes.addAndGet(readBytes)
                     return readBytes
                 }
@@ -81,70 +81,26 @@ class WADArchive(override val archiveFile: File) : IArchive {
 
     override fun compile(newEntries: List<Pair<String, () -> InputStream>>) {
         BackupManager.backupOverridingEntries(this, newEntries)
-        TODO()
 
-        //Check if can patch
-//        if (newEntries.all { (name, data) -> wad.files.any { entry -> entry.name == name && entry.size == data.size } }) {
-//            val wadFile = RandomAccessFile(archiveFile, "rw")
-//            val per = 100.0 / newEntries.size
-//
-//            newEntries.forEachIndexed { index, (name, data) ->
-//                if(SpiralModel.printCompilationPercentage)
-//                    println("Compilation Percentage: ${per * index}%")
-//
-//                val wadEntry = wad.files.first { entry -> entry.name == name && entry.size == data.size }
-//                wadFile.seek(wad.dataOffset + wadEntry.offset)
-//                data.use { stream -> stream.readChunked(processChunk = wadFile::write) }
-//            }
-//
-//            wadFile.close()
-//            trace("Patched!")
-//        } else {
-        var compiled = false
+        val cacheFiles = ArrayList<File>()
+        val customWad = customWAD {
+            add(wad)
+            newEntries.forEach { (name, data) ->
+                val cacheFile = CacheHandler.newCacheFile()
+                data().use { dataStream -> FileOutputStream(cacheFile).use { outStream -> dataStream.copyTo(outStream) } }
+                add(name, cacheFile)
+            }
+        }
 
-//            run reorganise@{
-//                if (wad.files.map { (name) -> name }.containsAll(newEntries.map { (name) -> name })) { //Ensure there's no new files
-//                    //If there's no new files, we can reorganise the WAD file based on priority.
-//                    //We won't do most of it here, but we do do priority checks here
-//
-//                    val priorities = wad.spiralPriorityList
-//
-//                    val highestPriority = priorities.map { (_, priority) -> priority }.max() ?: 0
-//                    val lowestPriority = newEntries.map { (name) -> priorities[name] ?: 0 }.min() ?: 0
-//
-//                    if (lowestPriority == 0 && highestPriority == 0) //All the files are here, but this is a fresh recompile.
-//                        return@reorganise
-//                    else if (lowestPriority > (highestPriority / 2)) { //Within "acceptable bounds"
-////                    val customWad = make<Custom> {
-////                        wad(wad)
-////                        newEntries.forEach { (name, data) -> this.data(name, data, prioritise = true) }
-////                    }
-//
-//                        val customWad = make<CustomPatchableWAD>(archiveFile) { newEntries.forEach { (name, data) -> this.data(name, data) } }
-//                        customWad.patch()
-//
-//                        compiled = true
-//                    } else
-//                        return@reorganise
-//                }
-//            }
+        val tmp = CacheHandler.newCacheFile()
 
-//        if (!compiled) {
-//            val customWad = make<CustomWAD> {
-//                add(wad)
-//                newEntries.forEach { (name, data) -> add(name, data.size, data::inputStream) }
-//            }
-//
-//            val tmp = File.createTempFile(UUID.randomUUID().toString(), ".wad")
-//            tmp.deleteOnExit()
-//
-//            try {
-//                FileOutputStream(tmp).use(customWad::compile)
-//                archiveFile.delete()
-//                tmp.renameTo(archiveFile)
-//            } finally {
-//                tmp.delete()
-//            }
-//        }
+        try {
+            FileOutputStream(tmp).use(customWad::compile)
+            archiveFile.delete()
+            tmp.renameTo(archiveFile)
+        } finally {
+            tmp.delete()
+            cacheFiles.forEach { file -> file.delete() }
+        }
     }
 }
