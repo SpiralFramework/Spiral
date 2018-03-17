dd101896d4ccaba00ef00b8da789e6702cbe21a5
Isaac <undermybrella@abimon.org>
2018-03-18 00:55:36 +1100

    Made some changes to allow for script execution

--- src/main/kotlin/org/abimon/spiral/mvc/Cockpit.kt
+++ src/main/kotlin/org/abimon/spiral/mvc/Cockpit.kt
@@ -7,6 +7,7 @@ import org.abimon.spiral.core.formats.images.SRDFormat
 import org.abimon.spiral.modding.ModManager
 import org.abimon.spiral.modding.PluginManager
 import org.abimon.spiral.mvc.gurren.*
+import java.io.File
 
 fun main(args: Array<String>) = startupSpiral(args)
 
@@ -30,14 +31,34 @@ fun startupSpiral(args: Array<String>) {
 
     println("Initialising SPIRAL")
 
+    val startingScope = SpiralModel.scope
+    val startingAutoConfirm = SpiralModel.autoConfirm
+    SpiralModel.scope = "> " to "default"
+    SpiralModel.autoConfirm = true
+
     args.forEach { param ->
-        if(param.startsWith("-Soperation=")) {
+        if(param.startsWith("-Soperation=") || param.startsWith("cmd=")) {
             val unknown = SpiralModel.imperator.dispatch(InstanceOrder<String>("STDIN", scout = null, data = param.split('=', limit = 2).last())).isEmpty()
             Thread.sleep(250)
             if(unknown)
                 println("Unknown command")
+        } else if (param.startsWith("cmd_file=")) {
+            val filename = param.split('=', limit = 2).last()
+            val file = File(filename)
+
+            if(file.exists())
+                file.useLines { lines -> lines.forEach { line ->
+                    val unknown = SpiralModel.imperator.dispatch(InstanceOrder<String>("STDIN", scout = null, data = line)).isEmpty()
+                    Thread.sleep(250)
+                    if(unknown)
+                        println("Unknown command")
+                }
+                }
         }
     }
+
+    SpiralModel.scope = startingScope
+    SpiralModel.autoConfirm = startingAutoConfirm
 
     while(Gurren.keepLooping) {
         try {
