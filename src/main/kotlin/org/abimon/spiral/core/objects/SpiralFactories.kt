2f239f3d13853b9d307bdb91ee1c8f55bd50bae9
Isaac <undermybrella@abimon.org>
2018-03-28 12:31:01 +1100

    Add CustomLin

--- src/main/kotlin/org/abimon/spiral/core/objects/SpiralFactories.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/SpiralFactories.kt
@@ -1,9 +1,11 @@
 package org.abimon.spiral.core.objects
 
+import org.abimon.spiral.core.objects.archives.CustomPak
 import org.abimon.spiral.core.objects.archives.CustomWAD
 import org.abimon.spiral.core.objects.archives.Pak
 import org.abimon.spiral.core.objects.archives.WAD
 import org.abimon.spiral.core.objects.game.hpa.HopesPeakDRGame
+import org.abimon.spiral.core.objects.scripting.CustomLin
 import org.abimon.spiral.core.objects.scripting.Lin
 import java.io.InputStream
 
@@ -16,4 +18,16 @@ fun customWAD(init: CustomWAD.() -> Unit): CustomWAD {
     val customWAD = CustomWAD()
     customWAD.init()
     return customWAD
+}
+
+fun customPak(init: CustomPak.() -> Unit): CustomPak {
+    val customPak = CustomPak()
+    customPak.init()
+    return customPak
+}
+
+fun customLin(init: CustomLin.() -> Unit): CustomLin {
+    val customLin = CustomLin()
+    customLin.init()
+    return customLin
 }
