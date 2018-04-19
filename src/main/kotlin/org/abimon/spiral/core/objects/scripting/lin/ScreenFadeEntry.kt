9f4941690e75d6e5629475b398d87dad51456102
Isaac <undermybrella@abimon.org>
2018-04-19 16:21:28 +1000

    Change op code for ScreenFadeEntry

--- src/main/kotlin/org/abimon/spiral/core/objects/scripting/lin/ScreenFadeEntry.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/scripting/lin/ScreenFadeEntry.kt
@@ -3,6 +3,6 @@ package org.abimon.spiral.core.objects.scripting.lin
 data class ScreenFadeEntry(val fadeIn: Boolean, val colour: Int, val frameDuration: Int): LinScript {
     constructor(opCode: Int, args: IntArray): this(args[0] == 0, args[1], args[2])
 
-    override val opCode: Int = 0x2A
+    override val opCode: Int = 0x22
     override val rawArguments: IntArray = intArrayOf(if(fadeIn) 0 else 1, colour, frameDuration)
 }
