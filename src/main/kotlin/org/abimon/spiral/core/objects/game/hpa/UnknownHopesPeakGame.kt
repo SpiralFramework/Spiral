bf242b7bcf194da719dfe82017b2985c638567df
Isaac <undermybrella@abimon.org>
2018-04-24 14:49:47 +1000

    Add the op files for the rest of the games

--- src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/UnknownHopesPeakGame.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/UnknownHopesPeakGame.kt
@@ -1,12 +1,30 @@
 package org.abimon.spiral.core.objects.game.hpa
 
 import org.abimon.spiral.core.objects.scripting.lin.LinScript
-import org.abimon.spiral.core.utils.OpCodeMap
+import org.abimon.spiral.core.objects.scripting.lin.UnknownEntry
+import org.abimon.spiral.core.utils.*
+import java.io.File
 import java.util.*
 
 object UnknownHopesPeakGame: HopesPeakDRGame {
     override val pakNames: Map<String, Array<String>> = emptyMap()
-    override val opCodes: OpCodeMap<IntArray, LinScript> = emptyMap()
+    override val opCodes: OpCodeMap<IntArray, LinScript> =
+                OpCodeHashMap<IntArray, LinScript>().apply {
+                    val opCodes = File("unk-ops.json")
+
+                    if (opCodes.exists()) {
+
+                        DataMapper.fileToMap(opCodes)?.forEach { opName, params ->
+                            val array = (params as? Array<*>)?.mapNotNull { any ->
+                                val str = any.toString()
+                                if (str.startsWith("0x"))
+                                    return@mapNotNull str.substring(2).toIntOrNull(16)
+                                return@mapNotNull str.toIntOrNull()
+                            } ?: return@forEach
+                            this[array[0]] = opName to array[1] and ::UnknownEntry
+                        }
+                    }
+                }
     override val customOpCodeArgumentReader: Map<Int, (LinkedList<Int>) -> IntArray> = emptyMap()
     override val characterIDs: Map<Int, String> = emptyMap()
     override val characterIdentifiers: MutableMap<String, Int> = HashMap<String, Int>()
