46faac7852228fee1ea999d8d3fdf9139477e763
Isaac <undermybrella@abimon.org>
2018-05-04 18:12:08 +1000

    Update OSL and SpiralFormats, and move SRDI model parsing over to SpiralFormats

--- src/main/kotlin/org/abimon/spiral/core/objects/models/SRDIMesh.kt
+++
@@ -1,7 +0,0 @@
-package org.abimon.spiral.core.objects.models
-
-data class SRDIMesh(
-        val vertices: List<Triple<Float, Float, Float>>,
-        val uvs: List<Pair<Float, Float>>,
-        val faces: List<Triple<Int, Int, Int>>
-)
