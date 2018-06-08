a6bf5ed5cc7e780cd87b3a9c4cfd975ecdc6de05
Isaac <undermybrella@abimon.org>
2018-06-08 15:13:34 +1000

    Fix WrdScript subclasses still containing an override for cmdArguments

--- src/main/kotlin/org/abimon/spiral/core/objects/scripting/wrd/LabelEntry.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/scripting/wrd/LabelEntry.kt
@@ -2,6 +2,5 @@ package org.abimon.spiral.core.objects.scripting.wrd
 
 data class LabelEntry(val labelID: Int): WrdScript {
     override val opCode: Int = 0x14
-    override val rawArguments: IntArray = intArrayOf(labelID shr 8, labelID % 256)
-    override val cmdArguments: IntArray = intArrayOf(labelID)
+    override val rawArguments: IntArray = intArrayOf(labelID)
 }
--- src/main/kotlin/org/abimon/spiral/core/objects/scripting/wrd/VoiceLineEntry.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/scripting/wrd/VoiceLineEntry.kt
@@ -2,6 +2,5 @@ package org.abimon.spiral.core.objects.scripting.wrd
 
 data class VoiceLineEntry(val voiceLine: Int, val volumeControl: Int): WrdScript {
     override val opCode: Int = 0x19
-    override val rawArguments: IntArray = intArrayOf(voiceLine shr 8, voiceLine % 256, volumeControl)
-    override val cmdArguments: IntArray = intArrayOf(voiceLine, volumeControl)
+    override val rawArguments: IntArray = intArrayOf(voiceLine, volumeControl)
 }
