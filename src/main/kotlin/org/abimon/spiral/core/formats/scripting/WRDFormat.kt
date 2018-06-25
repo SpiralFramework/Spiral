2d8fa3faf6739c0bcff095adabc56fb4e8a1d4e2
Isaac <undermybrella@abimon.org>
2018-06-25 15:22:16 +1000

    Update SpiralFormats, rework WRDFormat conversion (one way only atm)

--- src/main/kotlin/org/abimon/spiral/core/formats/scripting/WRDFormat.kt
+++ src/main/kotlin/org/abimon/spiral/core/formats/scripting/WRDFormat.kt
@@ -3,6 +3,7 @@ package org.abimon.spiral.core.formats.scripting
 import org.abimon.spiral.core.formats.SpiralFormat
 import org.abimon.spiral.core.objects.game.DRGame
 import org.abimon.spiral.core.objects.game.v3.V3
+import org.abimon.spiral.core.objects.scripting.EnumWordScriptCommand
 import org.abimon.spiral.core.objects.scripting.WordScriptFile
 import org.abimon.spiral.core.println
 import java.io.InputStream
@@ -37,25 +38,33 @@ object WRDFormat : SpiralFormat {
         output.println("Set Game To V3")
 
         //TODO: Re implement WRD conversion
-//        wrd.commandOneEntries.forEach { s -> output.println("Word Command 1: $s") }
-//        wrd.commandTwoEntries.forEach { s -> output.println("Word Command 2: $s") }
-//        wrd.commandThreeEntries.forEach { s -> output.println("Word Command 3: $s") }
-//        wrd.strings.forEach { str -> output.println("Word String: $str") }
-//
-//        wrd.entries.forEach { entry ->
-//            val op = SpiralData.drv3OpCodes[entry.opCode]?.second ?: "0x${entry.opCode.toString(16)}"
-//            when(entry) {
-//                is LabelEntry -> output.println("$op|${wrd.commandOneEntries[entry.labelID]}")
-//                is TextEntry -> output.println("$op|${entry.id}")
-//                else -> {
-//                    try {
-//                        output.println("$op|${entry.cmdArguments.joinToString { arg -> wrd.commandTwoEntries[arg] }}")
-//                    } catch(aioob: ArrayIndexOutOfBoundsException) {
-//                        output.println("$op|${entry.rawArguments.joinToString { arg -> "raw:$arg" }}")
-//                    }
-//                }
-//            }
-//        }
+        wrd.labels.forEach { s -> output.println("Word Command Label: $s") }
+        wrd.strings.forEach { s -> output.println("Word Command String: $s") }
+        wrd.parameters.forEach { s -> output.println("Word Command Parameter: $s") }
+
+        wrd.entries.forEach { entrySet ->
+            output.println(entrySet.joinToString("\n") { entry ->
+                val commandEntries = game.opCodeCommandEntries[entry.opCode]
+                val range = commandEntries?.indices ?: IntRange.EMPTY
+
+                val arguments = entry.rawArguments.mapIndexed { index, arg ->
+                    if (index in range) {
+                        return@mapIndexed when (commandEntries!![index]) {
+                            EnumWordScriptCommand.LABEL -> "LABEL:${wrd.labels[arg]}"
+                            EnumWordScriptCommand.PARAMETER -> "PARAMETER:${wrd.parameters[arg]}"
+                            EnumWordScriptCommand.STRING -> "STRING:${wrd.strings[arg]}"
+                            EnumWordScriptCommand.RAW -> arg.toString()
+                        }
+                    } else {
+                        return@mapIndexed arg.toString()
+                    }
+                }
+
+                return@joinToString "0x${entry.opCode.toString(16)}|${arguments.joinToString()}"
+            })
+
+            output.println("")
+        }
 
         return true
     }
