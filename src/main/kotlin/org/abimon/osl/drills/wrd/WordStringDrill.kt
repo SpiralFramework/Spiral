d4376ac15dd27d212a9fb8173289e46c85553b45
Isaac <undermybrella@abimon.org>
2018-06-26 16:04:37 +1000

    Add WordScriptString, which will allow for ID mapping with STX compilation

--- src/main/kotlin/org/abimon/osl/drills/wrd/WordStringDrill.kt
+++ src/main/kotlin/org/abimon/osl/drills/wrd/WordStringDrill.kt
@@ -1,24 +1,34 @@
 package org.abimon.osl.drills.wrd
 
 import org.abimon.osl.OpenSpiralLanguageParser
-import org.abimon.osl.WordScriptCommand
+import org.abimon.osl.WordScriptString
 import org.abimon.osl.drills.DrillHead
-import org.abimon.spiral.core.objects.scripting.EnumWordScriptCommand
 import org.parboiled.Rule
 import kotlin.reflect.KClass
 
-object WordStringDrill : DrillHead<WordScriptCommand> {
+object WordStringDrill : DrillHead<WordScriptString> {
     val cmd = "WRD-STRING"
 
-    override val klass: KClass<WordScriptCommand> = WordScriptCommand::class
+    override val klass: KClass<WordScriptString> = WordScriptString::class
     override fun OpenSpiralLanguageParser.syntax(): Rule =
             Sequence(
                     clearTmpStack(cmd),
 
                     Sequence(
                             "Word String",
-                            AnyOf(":|"),
                             pushDrillHead(cmd, this@WordStringDrill),
+                            FirstOf(
+                                    Sequence(
+                                            Whitespace(),
+                                            "ID",
+                                            AnyOf(":|"),
+                                            OptionalWhitespace(),
+                                            RuleWithVariables(OneOrMore(Digit())),
+                                            pushTmpFromStack(cmd)
+                                    ),
+                                    pushTmpAction(cmd, "-1")
+                            ),
+                            AnyOf(":|"),
                             OptionalInlineWhitespace(),
                             WrdText(cmd),
                             operateOnTmpActions(cmd) { stack -> operate(this, stack.drop(1).toTypedArray()) }
@@ -27,10 +37,11 @@ object WordStringDrill : DrillHead<WordScriptCommand> {
                     pushStackWithHead(cmd)
             )
 
-    override fun operate(parser: OpenSpiralLanguageParser, rawParams: Array<Any>): WordScriptCommand {
-        val string = rawParams[0].toString()
+    override fun operate(parser: OpenSpiralLanguageParser, rawParams: Array<Any>): WordScriptString {
+        val index = rawParams[0].toString().toIntOrNull() ?: -1
+        val string = rawParams[1].toString()
         if (string !in parser.wordScriptStrings)
             parser.wordScriptStrings.add(string)
-        return WordScriptCommand(EnumWordScriptCommand.STRING, string)
+        return WordScriptString(string, index)
     }
 }
