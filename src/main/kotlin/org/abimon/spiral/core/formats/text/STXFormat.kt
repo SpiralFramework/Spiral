f8ee5dce280d0316f32ceeb6e12d082ebbccef93
Isaac <undermybrella@abimon.org>
2018-06-28 17:03:15 +1000

    STX <-> OSL conversion support

--- src/main/kotlin/org/abimon/spiral/core/formats/text/STXFormat.kt
+++ src/main/kotlin/org/abimon/spiral/core/formats/text/STXFormat.kt
@@ -1,16 +1,17 @@
 package org.abimon.spiral.core.formats.text
 
 import org.abimon.spiral.core.formats.SpiralFormat
+import org.abimon.spiral.core.formats.scripting.OpenSpiralLanguageFormat
 import org.abimon.spiral.core.objects.game.DRGame
 import org.abimon.spiral.core.objects.text.STXT
 import org.abimon.spiral.core.println
 import java.io.InputStream
 import java.io.OutputStream
 
-object STXTFormat : SpiralFormat {
-    override val name = "STXT"
+object STXFormat : SpiralFormat {
+    override val name = "STX"
     override val extension = "stx"
-    override val conversions: Array<SpiralFormat> = arrayOf(TextFormat)
+    override val conversions: Array<SpiralFormat> = arrayOf(OpenSpiralLanguageFormat)
 
     override fun isFormat(game: DRGame?, name: String?, context: (String) -> (() -> InputStream)?, dataSource: () -> InputStream): Boolean {
         try {
@@ -23,10 +24,13 @@ object STXTFormat : SpiralFormat {
     override fun convert(game: DRGame?, format: SpiralFormat, name: String?, context: (String) -> (() -> InputStream)?, dataSource: () -> InputStream, output: OutputStream, params: Map<String, Any?>): Boolean {
         if(super.convert(game, format, name, context, dataSource, output, params)) return true
 
-        val stxt = STXT(dataSource)
+        val stx = STXT(dataSource)
 
-        output.println("STXT File\nLanguage: ${stxt.lang}")
-        stxt.strings.toSortedMap().forEach { id, str -> output.println("$id|$str") }
+        output.println("OSL Script")
+        output.println("Context: STX")
+        output.println("Language: ${stx.lang.name}")
+
+        stx.strings.toSortedMap().forEach { id, str -> output.println("Word String (ID is $id): ${str.replace("\n", "\\n")}") }
 
         return true
     }
--- src/main/kotlin/org/abimon/spiral/core/formats/text/TextFormat.kt
+++ src/main/kotlin/org/abimon/spiral/core/formats/text/TextFormat.kt
@@ -8,7 +8,7 @@ object TextFormat: SpiralFormat {
     override val name: String = "Text"
     override val extension: String = "txt"
     override val conversions: Array<SpiralFormat> = emptyArray() //We should not be doing any automated conversions
-    val manualConversions: Array<SpiralFormat> = arrayOf(STXTFormat) //But we should allow manual conversions
+    val manualConversions: Array<SpiralFormat> = arrayOf(STXFormat) //But we should allow manual conversions
 
     override fun canConvert(game: DRGame?, format: SpiralFormat): Boolean = format in manualConversions
     override fun isFormat(game: DRGame?, name: String?, context: (String) -> (() -> InputStream)?, dataSource: () -> InputStream): Boolean = true
