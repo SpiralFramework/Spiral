e323bcada60e6ba224c71d28f74171a295343d86
Isaac <undermybrella@abimon.org>
2018-07-01 11:50:29 +1000

    Finalise compilation of CustomNonstopDebate's

--- src/main/kotlin/org/abimon/spiral/core/objects/scripting/CustomNonstopDebate.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/scripting/CustomNonstopDebate.kt
@@ -1,33 +1,43 @@
 package org.abimon.spiral.core.objects.scripting
 
 import org.abimon.spiral.core.objects.ICompilable
+import org.abimon.spiral.core.objects.game.hpa.DR1
+import org.abimon.spiral.core.objects.game.hpa.DR2
 import org.abimon.spiral.core.objects.game.hpa.HopesPeakDRGame
 import org.abimon.spiral.core.objects.game.hpa.UnknownHopesPeakGame
 import org.abimon.spiral.core.utils.writeInt16LE
 import java.io.OutputStream
 
-class CustomNonstopDebate {
-//    override val dataSize: Long
-//        get() = 4 + (sections.size * )
-//    val sections: MutableList = ArrayList<NonstopDebate>()
-//    var game: HopesPeakDRGame = UnknownHopesPeakGame
-//        set(value) {
-//            field = value
-//
-//            when (field) {
-//
-//            }
-//        }
-//    var timeLimit = 300
-//
-//    private var bytesPerSection: Int = 0
-//
-//    fun section(section: NonstopDebateSection) = sections.add(section)
-//
-//    fun compile(output: OutputStream) {
-//        output.writeInt16LE(timeLimit)
-//        output.writeInt16LE(sections.size)
-//
-//        sections.forEach { it.data.forEachIndexed { index, num -> if(index * 2 <= bytesPerSection) output.writeShort(num.toLong()) } }
-//    }
+class CustomNonstopDebate: ICompilable {
+    override val dataSize: Long
+        get() = 4L + (sections.size * bytesPerSection)
+
+    val sections: MutableList<NonstopDebateSection> = ArrayList()
+    var bytesPerSection: Int = 0
+
+    var game: HopesPeakDRGame = UnknownHopesPeakGame
+        set(value) {
+            field = value
+
+            when (field) {
+                DR1 -> bytesPerSection = 60
+                DR2 -> bytesPerSection = 68
+            }
+        }
+
+    var timeLimit = 300
+
+    fun section(section: NonstopDebateSection) { sections.add(section) }
+
+    override fun compile(output: OutputStream) {
+        output.writeInt16LE(timeLimit)
+        output.writeInt16LE(sections.size)
+
+        sections.forEach { section ->
+            section.data.forEachIndexed { index, num ->
+                if(index * 2 <= bytesPerSection)
+                    output.writeInt16LE(num.toLong())
+            }
+        }
+    }
 }
