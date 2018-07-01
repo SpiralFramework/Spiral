a480eb165d8dabaffe4295a91e3734a9790917e6
Isaac <undermybrella@abimon.org>
2018-07-01 12:10:45 +1000

    Add Nonstop Debate Op Code Names

---
+++ src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/HopesPeakKillingGame.kt
@@ -0,0 +1,8 @@
+package org.abimon.spiral.core.objects.game.hpa
+
+/**
+ * The Hope's Peak arc of killing games
+ */
+interface HopesPeakKillingGame: HopesPeakDRGame {
+    val nonstopDebateOpCodeNames: Map<Int, String>
+}
