0f142feffb9400802cb1a4a9d61f6f69fc515294
Isaac <undermybrella@abimon.org>
2018-09-19 18:14:03 +1000

    Overhaul arithmetic to support op code addition

--- src/main/kotlin/org/abimon/osl/EnumArithmetic.kt
+++ src/main/kotlin/org/abimon/osl/EnumArithmetic.kt
@@ -1,6 +1,9 @@
 package org.abimon.osl
 
+import org.abimon.spiral.core.objects.game.hpa.DR1
+import org.abimon.spiral.core.objects.game.hpa.DR2
 import org.abimon.spiral.core.objects.scripting.lin.LinScript
+import org.abimon.spiral.core.objects.scripting.lin.UnknownEntry
 
 enum class EnumArithmetic(private val perform: (Int, Int) -> Int, vararg val names: String) {
     PLUS(Int::plus, "plus", "+", "add", "+="),
@@ -16,5 +19,19 @@ enum class EnumArithmetic(private val perform: (Int, Int) -> Int, vararg val nam
         val NAMES: Array<String> by lazy { values().flatMap{ enum -> enum.names.toList() }.toTypedArray() }
     }
 
-    operator fun invoke(parser: OpenSpiralLanguageParser, variable: Int, amount: Int): Array<LinScript> = ArithmeticOperations.operate(parser, variable, amount, perform)
+    operator fun invoke(parser: OpenSpiralLanguageParser, variable: Int, amount: Int): Array<LinScript> {
+        return when (this) {
+            PLUS -> when (parser.hopesPeakGame) {
+                DR1 -> arrayOf(UnknownEntry(0x33, intArrayOf(variable, 1, (amount shr 8) and 0xFF, amount and 0xFF)) as LinScript)
+                DR2 -> arrayOf(UnknownEntry(0x3A, intArrayOf(variable, 1, (amount shr 8) and 0xFF, amount and 0xFF)) as LinScript)
+                else -> ArithmeticOperations.operate(parser, variable, amount, perform)
+            }
+            MINUS -> when (parser.hopesPeakGame) {
+                DR1 -> arrayOf(UnknownEntry(0x33, intArrayOf(variable, 2, (amount shr 8) and 0xFF, amount and 0xFF)) as LinScript)
+                DR2 -> arrayOf(UnknownEntry(0x3A, intArrayOf(variable, 2, (amount shr 8) and 0xFF, amount and 0xFF)) as LinScript)
+                else -> ArithmeticOperations.operate(parser, variable, amount, perform)
+            }
+            else -> ArithmeticOperations.operate(parser, variable, amount, perform)
+        }
+    }
 }
