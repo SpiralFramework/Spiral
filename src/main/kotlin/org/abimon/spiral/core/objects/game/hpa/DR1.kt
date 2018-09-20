67d6192530943ba27ca60222d064d3068a33af3b
Isaac <undermybrella@abimon.org>
2018-09-21 01:25:27 +1000

    Add Trial Camera names

--- src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/DR1.kt
+++ src/main/kotlin/org/abimon/spiral/core/objects/game/hpa/DR1.kt
@@ -305,6 +305,7 @@ object DR1 : HopesPeakKillingGame {
     }
 
     override val nonstopDebateSectionSize: Int = 60
+    override val trialCameraNames: Array<String> = SharedHPA.trialCameraNames["dr2"] ?: emptyArray()
 
     fun readCheckFlagA(stream: LinkedList<Int>): IntArray {
         val args: MutableList<Int> = ArrayList()
