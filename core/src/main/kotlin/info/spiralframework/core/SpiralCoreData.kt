e8e8f253c7aa95645f7efcdd6c5d2fe351034f41
Isaac <undermybrella@abimon.org>
2018-12-18 02:23:26 +1100

    Add JSON, YAML, and XML mappers to SpiralCoreData, and InstantSerialisation

---
+++ core/src/main/kotlin/info/spiralframework/core/SpiralCoreData.kt
@@ -0,0 +1,66 @@
+package info.spiralframework.core
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
+import info.spiralframework.core.serialisation.InstantSerialisation
+
+/**
+ * This singleton holds important information for all Spiral modules
+ * @author UnderMybrella
+ * @since 1.0.0-alpha
+ */
+object SpiralCoreData {
+    /** Jackson mapper for JSON data */
+    val JSON_MAPPER: ObjectMapper = ObjectMapper()
+            .registerKotlinModule()
+            .registerModules(Jdk8Module(), JavaTimeModule(), ParameterNamesModule())
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET)
+            .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+
+    /** Jackson mapper for YAML data */
+    val YAML_MAPPER: ObjectMapper = ObjectMapper(YAMLFactory())
+            .registerKotlinModule()
+            .registerModules(Jdk8Module(), JavaTimeModule(), ParameterNamesModule())
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET)
+            .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+
+    /** Jackson mapper for XML data */
+    val XML_MAPPER: ObjectMapper = XmlMapper(JacksonXmlModule().apply { setDefaultUseWrapper(false) })
+            .registerKotlinModule()
+            .registerModules(Jdk8Module(), JavaTimeModule(), ParameterNamesModule(), InstantSerialisation.MODULE())
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET)
+            .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+            .setDefaultSetterInfo(JsonSetter.Value.forValueNulls(Nulls.AS_EMPTY))
+
+    /** Steam ID for Danganronpa: Trigger Happy Havoc */
+    val STEAM_DANGANRONPA_TRIGGER_HAPPY_HAVOC = "413410"
+    /** Steam ID for Danganronpa 2: Goodbye Despair */
+    val STEAM_DANGANRONPA_2_GOODBYE_DESPAIR = "413420"
+
+    /** 'File Name' for Spiral header data, to be used in archives */
+    val SPIRAL_HEADER_NAME = "Spiral-Header"
+    /**
+     * 'File Name' for Spiral mod list data, to be used in archives
+     * This file should ideally keep track of mods currently installed, and their files + versions
+     * */
+    val SPIRAL_MOD_LIST = "Spiral-Mod-List"
+}
