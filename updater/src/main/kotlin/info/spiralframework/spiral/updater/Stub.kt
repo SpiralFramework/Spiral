8e48c8b29b5d56568c91f7aa43d6af161883c2ff
Isaac <undermybrella@abimon.org>
2019-02-01 20:38:05 +1100

    Console now has a temporary update utility (will flesh out later)

--- updater/src/main/kotlin/info/spiralframework/spiral/updater/Stub.kt
+++ updater/src/main/kotlin/info/spiralframework/spiral/updater/Stub.kt
@@ -1,18 +1,29 @@
 package info.spiralframework.spiral.updater
 
 import java.io.File
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Paths
 
 object Stub {
     @JvmStatic
     fun main(args: Array<String>) {
-        val javaHome = System.getProperty("java.home")
-        val javaBin = javaHome +
-                File.separator + "bin" +
-                File.separator + "java"
+        Thread.sleep(500)
 
-        val codeSource = Stub::class.java.jarLocation
-        ProcessBuilder(javaBin, "-cp", codeSource.toURI().toString(), *args)
-                .inheritIO()
-                .start()
+        if (args[0] == DELETE_ARG) {
+            Files.delete(Paths.get(URI(args[0])))
+
+            return
+        } else {
+            val javaHome = System.getProperty("java.home")
+            val javaBin = javaHome +
+                    File.separator + "bin" +
+                    File.separator + "java"
+
+            val codeSource = Stub::class.java.jarLocation
+            ProcessBuilder(javaBin, "-cp", codeSource.toURI().toString(), *args)
+                    .inheritIO()
+                    .start()
+        }
     }
 }
