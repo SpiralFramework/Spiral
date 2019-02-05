1f1f5853f3308fb4b8df688cc8adf4f283bdcff5
Izzy <undermybrella@abimon.org>
2019-02-05 16:14:58 +1100

    Register shutdown hook

--- console/src/main/kotlin/info/spiralframework/console/CockpitUpdate.kt
+++ console/src/main/kotlin/info/spiralframework/console/CockpitUpdate.kt
@@ -10,8 +10,13 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import java.io.File
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
 
 class CockpitUpdate internal constructor(val updateFile: File, args: GurrenArgs, vararg rawArgs: String): Cockpit<CockpitUpdate>(args) {
+    companion object {
+        val needsMove = AtomicBoolean(false)
+    }
     val process: Process = ProcessBuilder("java", "-jar", updateFile.absolutePath, "--${GurrenArgs.DISABLE_UPDATE_CHECK}", *rawArgs)
             .inheritIO()
             .start()
@@ -26,11 +31,20 @@ class CockpitUpdate internal constructor(val updateFile: File, args: GurrenArgs,
                 //Move the update over
                 printlnLocale("gurren.update.moving")
                 moveUpdate(updateFile.toURI(), Cockpit::class.java.jarLocation.toURI(), locale("gurren.update.finished_moving"))
+                needsMove.set(false)
             }
         }
     }
 
     init {
         printlnLocale("gurren.update.init")
+
+        Runtime.getRuntime().addShutdownHook(thread(false) {
+            if (needsMove.get()) {
+                printlnLocale("gurren.update.moving")
+                moveUpdate(updateFile.toURI(), Cockpit::class.java.jarLocation.toURI(), locale("gurren.update.finished_moving"))
+                needsMove.set(false)
+            }
+        })
     }
 }
