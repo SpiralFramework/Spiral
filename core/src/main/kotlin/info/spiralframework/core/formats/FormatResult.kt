96ec2614bb871adb515d0b9f10711884cacab8b1
Isaac <undermybrella@abimon.org>
2019-03-10 12:10:33 +1100

    Use internal methods for reading to allow for wrapped identify + read

--- core/src/main/kotlin/info/spiralframework/core/formats/FormatResult.kt
+++ core/src/main/kotlin/info/spiralframework/core/formats/FormatResult.kt
@@ -8,47 +8,73 @@ sealed class FormatResult<T>: Closeable {
     companion object {
         operator fun <T> invoke(format: SpiralFormat? = null, obj: T?, isFormat: Boolean, chance: Double): FormatResult<T> {
             if (obj == null)
-                return Fail(format, chance)
+                return Fail<T>(chance).withFormat(format)
             if (!isFormat)
-                return Fail(format, chance)
-            return Success(format, obj, chance)
+                return Fail<T>(chance).withFormat(format)
+            return Success(obj, chance).withFormat(format)
         }
 
         operator fun <T> invoke(format: SpiralFormat? = null, obj: T?, chance: Double): FormatResult<T> {
             if (obj == null)
-                return Fail(format, chance)
-            return Success(format, obj, chance)
+                return Fail<T>(chance).withFormat(format)
+            return Success(obj, chance).withFormat(format)
+        }
+
+        operator fun <T> invoke(obj: T?, isFormat: Boolean, chance: Double): FormatResult<T> {
+            if (obj == null)
+                return Fail(chance)
+            if (!isFormat)
+                return Fail(chance)
+            return Success(obj, chance)
+        }
+
+        operator fun <T> invoke(obj: T?, chance: Double): FormatResult<T> {
+            if (obj == null)
+                return Fail(chance)
+            return Success(obj, chance)
+        }
+
+        val NO_FORMAT_DEFINED = object: SpiralFormat {
+            override val name: String = "[NO FORMAT DEFINED]"
         }
     }
 
-    class Success<T>(override val format: SpiralFormat?, override val obj: T, override val chance: Double): FormatResult<T>() {
+    class Success<T>(override val obj: T, override val chance: Double): FormatResult<T>() {
         override val didSucceed: Boolean = true
 
-        override fun <R> map(transform: (T) -> R): FormatResult<R> = Success(format, transform(obj), chance)
+        override fun <R> map(transform: (T) -> R): FormatResult<R> = Success(transform(obj), chance).withFormat(format)
         override fun filter(predicate: (T) -> Boolean): FormatResult<T> {
             if (predicate(obj))
                 return this
             else
-                return Fail(format, 1.0)
+                return Fail<T>(1.0).withFormat(format)
         }
-        override fun weight(predicate: (T) -> Double): FormatResult<T> = Success(format, obj, predicate(obj))
+        override fun weight(predicate: (T) -> Double): FormatResult<T> = Success(obj, predicate(obj)).withFormat(format)
     }
-    class Fail<T>(override val format: SpiralFormat?, override val chance: Double, val reason: Throwable? = null): FormatResult<T>() {
+    class Fail<T>(override val chance: Double, val reason: Throwable? = null): FormatResult<T>() {
         override val obj: T
             get() = throw NoSuchElementException("No value present")
         override val didSucceed: Boolean = false
 
-        override fun <R> map(transform: (T) -> R): FormatResult<R> = Fail(format, chance)
+        override fun <R> map(transform: (T) -> R): FormatResult<R> = Fail<R>(chance).withFormat(format)
         override fun filter(predicate: (T) -> Boolean): FormatResult<T> = this
         override fun weight(predicate: (T) -> Double): FormatResult<T> = this
     }
 
-    abstract val format: SpiralFormat?
     abstract val obj: T
     abstract val chance: Double
 
     abstract val didSucceed: Boolean
 
+    var format: SpiralFormat = NO_FORMAT_DEFINED
+    var safeFormat: SpiralFormat?
+        get() = if (format === NO_FORMAT_DEFINED) null else format
+        set(value) {
+            if (value == null)
+                format = NO_FORMAT_DEFINED
+            else
+                format = value
+        }
     val safeObj: T?
         get() = if (didSucceed) obj else null
 
