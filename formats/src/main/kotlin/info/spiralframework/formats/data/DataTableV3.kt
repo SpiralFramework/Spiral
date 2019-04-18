486f1c5355907ca8a29da95d042397826f889773
Isaac <undermybrella@abimon.org>
2019-04-18 15:11:04 +1000

    Parse most of the data files

--- formats/src/main/kotlin/info/spiralframework/formats/data/DataTableV3.kt
+++ formats/src/main/kotlin/info/spiralframework/formats/data/DataTableV3.kt
@@ -1,9 +1,7 @@
 package info.spiralframework.formats.data
 
 import info.spiralframework.base.CountingInputStream
-import info.spiralframework.base.util.assertAsLocaleArgument
-import info.spiralframework.base.util.readInt32LE
-import info.spiralframework.base.util.readNullTerminatedUTF8String
+import info.spiralframework.base.util.*
 import info.spiralframework.formats.utils.DataHandler
 import info.spiralframework.formats.utils.DataSource
 import info.spiralframework.formats.utils.align
@@ -25,24 +23,24 @@ class DataTableV3(val dataSource: DataSource) {
     }
 
     data class DataVariableHeader(val variableName: String, val variableType: String)
-    sealed class DataVariable<T>(val variableName: String, val data: T) {
-        class UnsignedByte(variableName: String, data: UByte): DataVariable<UByte>(variableName, data)
-        class UnsignedShort(variableName: String, data: UShort): DataVariable<UShort>(variableName, data)
-        class UnsignedInt(variableName: String, data: UInt): DataVariable<UInt>(variableName, data)
-        class UnsignedLong(variableName: String, data: ULong): DataVariable<ULong>(variableName, data)
+    sealed class DataVariable<T>(variableName: String, data: T) {
+        data class UnsignedByte(val variableName: String, val data: UByte): DataVariable<UByte>(variableName, data)
+        data class UnsignedShort(val variableName: String, val data: UShort): DataVariable<UShort>(variableName, data)
+        data class UnsignedInt(val variableName: String, val data: UInt): DataVariable<UInt>(variableName, data)
+        data class UnsignedLong(val variableName: String, val data: ULong): DataVariable<ULong>(variableName, data)
 
-        class SignedByte(variableName: String, data: Byte): DataVariable<Byte>(variableName, data)
-        class SignedShort(variableName: String, data: Short): DataVariable<Short>(variableName, data)
-        class SignedInt(variableName: String, data: Int): DataVariable<Int>(variableName, data)
-        class SignedLong(variableName: String, data: Long): DataVariable<Long>(variableName, data)
+        data class SignedByte(val variableName: String, val data: Byte): DataVariable<Byte>(variableName, data)
+        data class SignedShort(val variableName: String, val data: Short): DataVariable<Short>(variableName, data)
+        data class SignedInt(val variableName: String, val data: Int): DataVariable<Int>(variableName, data)
+        data class SignedLong(val variableName: String, val data: Long): DataVariable<Long>(variableName, data)
 
-        class Float(variableName: String, data: Float): DataVariable<Float>(variableName, data)
-        class Double(variableName: String, data: Double): DataVariable<Double>(variableName, data)
+        data class Float32(val variableName: String, val data: Float): DataVariable<Float>(variableName, data)
+        data class Float64(val variableName: String, val data: Double): DataVariable<Double>(variableName, data)
 
-        class Label(variableName: String, data: Int): DataVariable<Int>(variableName, data)
-        class Refer(variableName: String, data: Int): DataVariable<Int>(variableName, data)
-        class Ascii(variableName: String, data: Int): DataVariable<Int>(variableName, data)
-        class UTF16(variableName: String, data: Int): DataVariable<Int>(variableName, data)
+        data class Label(val variableName: String, val data: Int): DataVariable<Int>(variableName, data)
+        data class Refer(val variableName: String, val data: Int): DataVariable<Int>(variableName, data)
+        data class Ascii(val variableName: String, val data: Int): DataVariable<Int>(variableName, data)
+        data class UTF16(val variableName: String, val data: Int): DataVariable<Int>(variableName, data)
     }
 
     val structureCount: Int
@@ -50,7 +48,10 @@ class DataTableV3(val dataSource: DataSource) {
     val variableCount: Int
 
     val variableDetails: Array<DataVariableHeader>
-//    val entries: Array<Array<DataVariable<*>>>
+    val entries: Array<Array<DataVariable<*>>>
+
+    val utf8Strings: Array<String>
+    val utf16Strings: Array<String>
 
     init {
         val stream = CountingInputStream(dataSource())
@@ -75,14 +76,38 @@ class DataTableV3(val dataSource: DataSource) {
 
             stream.skip(stream.count.align(0x10).toLong())
 
-//            entries = Array(structureCount) {
-//                variableDetails.map { (variableName, variableType) ->
-//                    return when (variableType.toLowerCase()) {
-//                        "u8" -> DataVariable.UnsignedByte(variableName, stream.read().toUByte())
-//
-//                    }
-//                }
-//            }
+            entries = Array(structureCount) {
+                variableDetails.map { (variableName, variableType) ->
+                    return@map when (variableType.toLowerCase()) {
+                        "u8" -> DataVariable.UnsignedByte(variableName, stream.read().toUByte())
+                        "u16" -> DataVariable.UnsignedShort(variableName, stream.readInt16LE().toUShort())
+                        "u32" -> DataVariable.UnsignedInt(variableName, stream.readUInt32LE())
+                        "u64" -> DataVariable.UnsignedLong(variableName, stream.readUInt64LE())
+
+                        "s8" -> DataVariable.SignedByte(variableName, stream.read().toByte())
+                        "s16" -> DataVariable.SignedShort(variableName, stream.readInt16LE().toShort())
+                        "s32" -> DataVariable.SignedInt(variableName, stream.readInt32LE())
+                        "s64" -> DataVariable.SignedLong(variableName, stream.readInt64LE())
+
+                        "f32" -> DataVariable.Float32(variableName, stream.readFloat32LE())
+                        "f64" -> DataVariable.Float64(variableName, stream.readFloat64LE())
+
+                        "label" -> DataVariable.Label(variableName, stream.readInt16LE())
+                        "refer" -> DataVariable.Refer(variableName, stream.readInt16LE())
+                        "ascii" -> DataVariable.Ascii(variableName, stream.readInt16LE())
+                        "utf16" -> DataVariable.UTF16(variableName, stream.readInt16LE())
+
+                        else -> throw IllegalArgumentException("")
+                    } as DataVariable<*>
+                }.toTypedArray()
+            }
+
+            val utf8Count = stream.readInt16LE()
+            val utf16Count = stream.readInt16LE()
+
+            utf8Strings = Array(utf8Count) { stream.readNullTerminatedString(encoding = Charsets.UTF_8, bytesPer = 1) }
+            stream.skip(stream.count.align(2).toLong())
+            utf16Strings = Array(utf16Count) { stream.readNullTerminatedString(encoding = Charsets.UTF_16, bytesPer = 2) }
         } finally {
             stream.close()
         }
