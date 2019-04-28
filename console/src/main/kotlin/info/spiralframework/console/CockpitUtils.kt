b651bb35b29853350e3387d4c1ed64a0b7bf0eae
Isaac H <undermybrella@abimon.org>
2019-04-28 13:24:14 +1000

    Add unregistering

--- console/src/main/kotlin/info/spiralframework/console/CockpitUtils.kt
+++ console/src/main/kotlin/info/spiralframework/console/CockpitUtils.kt
@@ -2,6 +2,7 @@ package info.spiralframework.console
 
 import info.spiralframework.console.eventbus.ParboiledCommand
 import info.spiralframework.console.eventbus.RegisterCommandRequest
+import info.spiralframework.console.eventbus.UnregisterCommandRequest
 import org.greenrobot.eventbus.EventBus
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
@@ -12,5 +13,13 @@ fun <T: ParboiledCommand> EventBus.registerCommandClassViaRequest(commandRegistr
         @Suppress("UNCHECKED_CAST")
         if((recruit.returnType.classifier as? KClass<*>)?.isSubclassOf(registerSubclass) == true || recruit.returnType.classifier == registerSubclass)
             post(RegisterCommandRequest(recruit.get(commandRegistry) as T))
+    }
+}
+
+fun <T: ParboiledCommand> EventBus.unregisterCommandClassViaRequest(commandRegistry: Any, registerSubclass: KClass<in T> = ParboiledCommand::class) {
+    commandRegistry.javaClass.kotlin.memberProperties.forEach { recruit ->
+        @Suppress("UNCHECKED_CAST")
+        if((recruit.returnType.classifier as? KClass<*>)?.isSubclassOf(registerSubclass) == true || recruit.returnType.classifier == registerSubclass)
+            post(UnregisterCommandRequest(recruit.get(commandRegistry) as T))
     }
 }
