efc1bfcd15359cc5a24f1198ef919fb0da5b2cc6
Isaac H <undermybrella@abimon.org>
2019-04-28 13:16:27 +1000

    Add EventBus#registerCommandClassViaRequest

---
+++ console/src/main/kotlin/info/spiralframework/console/CockpitUtils.kt
@@ -0,0 +1,16 @@
+package info.spiralframework.console
+
+import info.spiralframework.console.eventbus.ParboiledCommand
+import info.spiralframework.console.eventbus.RegisterCommandRequest
+import org.greenrobot.eventbus.EventBus
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.memberProperties
+
+fun <T: ParboiledCommand> EventBus.registerCommandClassViaRequest(commandRegistry: Any, registerSubclass: KClass<in T> = ParboiledCommand::class) {
+    commandRegistry.javaClass.kotlin.memberProperties.forEach { recruit ->
+        @Suppress("UNCHECKED_CAST")
+        if((recruit.returnType.classifier as? KClass<*>)?.isSubclassOf(registerSubclass) == true || recruit.returnType.classifier == registerSubclass)
+            post(RegisterCommandRequest(recruit.get(commandRegistry) as T))
+    }
+}
