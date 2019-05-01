cd6791e050e4cf157ebbe5cb749e89b8489ad096
Isaac H <undermybrella@abimon.org>
2019-05-01 13:36:06 +1000

    In the event of compression, actually decompress the data

--- formats/src/main/kotlin/info/spiralframework/formats/archives/CPKFileEntry.kt
+++ formats/src/main/kotlin/info/spiralframework/formats/archives/CPKFileEntry.kt
@@ -13,37 +13,30 @@ import java.io.FileOutputStream
 import java.io.InputStream
 
 data class CPKFileEntry(val fileName: String, val directoryName: String, val fileSize: Long, val extractSize: Long, val offset: Long, val isCompressed: Boolean, val cpk: CPK) {
-    private val compressedFile: File by lazy {
+    private val compressedFile: File? by lazy {
         val hash = ".sha512-${rawInputStream.use(InputStream::sha512Hash)}"
-        DataHandler.createTmpFile(hash)
+        val file = DataHandler.createTmpFile(hash)
+
+        rawInputStream.use { baseStream ->
+            val magic = baseStream.readInt64LE()
+            if (magic != CRILAYLACompression.MAGIC_NUMBER) {
+                DataHandler.LOGGER.warn("formats.cpk.wrong_compression", "$directoryName/$fileName", "0x${magic.toString(16)}", "0x${CRILAYLACompression.MAGIC_NUMBER.toString(16)}")
+                return@use null
+            } else {
+                val uncompressedSize = baseStream.readInt32LE()
+                val dataSize = baseStream.readInt32LE()
+
+                val compressedData = baseStream.readXBytes(dataSize)
+                val rawDataHeader = baseStream.readXBytes(0x100)
+
+                FileOutputStream(file).use { outStream -> outStream.write(CRILAYLACompression.decompress(uncompressedSize, dataSize, compressedData, rawDataHeader)) }
+                return@use file
+            }
+        }
     }
 
     val inputStream: InputStream
-        get() {
-            if (isCompressed) {
-                if (!compressedFile.exists()) {
-                    rawInputStream.use { baseStream ->
-                        val magic = baseStream.readInt64LE()
-                        if (magic != CRILAYLACompression.MAGIC_NUMBER) {
-                            DataHandler.LOGGER.warn("formats.cpk.wrong_compression", "$directoryName/$fileName", "0x${magic.toString(16)}", "0x${CRILAYLACompression.MAGIC_NUMBER.toString(16)}")
-                        } else {
-                            val uncompressedSize = baseStream.readInt32LE()
-                            val dataSize = baseStream.readInt32LE()
-
-                            val compressedData = baseStream.readXBytes(dataSize)
-                            val rawDataHeader = baseStream.readXBytes(0x100)
-
-                            FileOutputStream(compressedFile).use { outStream -> outStream.write(CRILAYLACompression.decompress(uncompressedSize, dataSize, compressedData, rawDataHeader)) }
-                        }
-                    }
-                }
-
-                if (compressedFile.exists())
-                    return FileInputStream(compressedFile)
-            }
-
-            return rawInputStream
-        }
+        get() = compressedFile?.let(::FileInputStream) ?: rawInputStream
 
     val rawInputStream: InputStream
         get() = WindowedInputStream(cpk.dataSource(), offset, fileSize)
