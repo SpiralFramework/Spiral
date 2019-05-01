87e4f71c4ba2be6301f6f441a92fd8643ebfa7dd
Isaac H <undermybrella@abimon.org>
2019-05-01 12:34:09 +1000

    Cache CPK data

--- formats/src/main/kotlin/info/spiralframework/formats/archives/CPKFileEntry.kt
+++ formats/src/main/kotlin/info/spiralframework/formats/archives/CPKFileEntry.kt
@@ -4,30 +4,42 @@ import info.spiralframework.base.WindowedInputStream
 import info.spiralframework.base.util.readInt32LE
 import info.spiralframework.base.util.readInt64LE
 import info.spiralframework.base.util.readXBytes
+import info.spiralframework.base.util.sha512Hash
 import info.spiralframework.formats.compression.CRILAYLACompression
 import info.spiralframework.formats.utils.DataHandler
-import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.InputStream
 
 data class CPKFileEntry(val fileName: String, val directoryName: String, val fileSize: Long, val extractSize: Long, val offset: Long, val isCompressed: Boolean, val cpk: CPK) {
+    private val compressedFile: File by lazy {
+        val hash = ".sha512-${rawInputStream.use(InputStream::sha512Hash)}"
+        DataHandler.createTmpFile(hash)
+    }
+
     val inputStream: InputStream
         get() {
-            //TODO: Figure out a better way to cache this data
             if (isCompressed) {
-                rawInputStream.use { baseStream ->
-                    val magic = baseStream.readInt64LE()
-                    if (magic != CRILAYLACompression.MAGIC_NUMBER) {
-                        DataHandler.LOGGER.warn("formats.cpk.wrong_compression", "$directoryName/$fileName", "0x${magic.toString(16)}", "0x${CRILAYLACompression.MAGIC_NUMBER.toString(16)}")
-                    } else {
-                        val uncompressedSize = baseStream.readInt32LE()
-                        val dataSize = baseStream.readInt32LE()
+                if (!compressedFile.exists()) {
+                    rawInputStream.use { baseStream ->
+                        val magic = baseStream.readInt64LE()
+                        if (magic != CRILAYLACompression.MAGIC_NUMBER) {
+                            DataHandler.LOGGER.warn("formats.cpk.wrong_compression", "$directoryName/$fileName", "0x${magic.toString(16)}", "0x${CRILAYLACompression.MAGIC_NUMBER.toString(16)}")
+                        } else {
+                            val uncompressedSize = baseStream.readInt32LE()
+                            val dataSize = baseStream.readInt32LE()
 
-                        val compressedData = baseStream.readXBytes(dataSize)
-                        val rawDataHeader = baseStream.readXBytes(0x100)
+                            val compressedData = baseStream.readXBytes(dataSize)
+                            val rawDataHeader = baseStream.readXBytes(0x100)
 
-                        return ByteArrayInputStream(CRILAYLACompression.decompress(uncompressedSize, dataSize, compressedData, rawDataHeader))
+                            FileOutputStream(compressedFile).use { outStream -> outStream.write(CRILAYLACompression.decompress(uncompressedSize, dataSize, compressedData, rawDataHeader)) }
+                        }
                     }
                 }
+
+                if (compressedFile.exists())
+                    return FileInputStream(compressedFile)
             }
 
             return rawInputStream
--- formats/src/main/kotlin/info/spiralframework/formats/utils/DataHandler.kt
+++ formats/src/main/kotlin/info/spiralframework/formats/utils/DataHandler.kt
@@ -24,6 +24,7 @@ object DataHandler {
 
     val tmpFiles: MutableSet<File> = CopyOnWriteArraySet()
 
+    //TODO: Figure out a better way to cache this data
     fun createTmpFile(hash: String): File {
         val tmp = File.createTempFile(hash, ".dat")
         tmp.deleteOnExit()
