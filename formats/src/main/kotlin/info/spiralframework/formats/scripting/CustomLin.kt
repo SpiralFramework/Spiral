a3a012b234980e63a49161a90d4a23d3bb6ccb01
Isaac H <undermybrella@abimon.org>
2019-06-22 16:35:56 +1000

    De-duplicate strings in lin files

--- formats/src/main/kotlin/info/spiralframework/formats/scripting/CustomLin.kt
+++ formats/src/main/kotlin/info/spiralframework/formats/scripting/CustomLin.kt
@@ -1,36 +1,52 @@
 package info.spiralframework.formats.scripting
 
+import info.spiralframework.base.util.writeInt32LE
 import info.spiralframework.formats.scripting.lin.LinScript
 import info.spiralframework.formats.scripting.lin.LinTextScript
 import info.spiralframework.formats.scripting.lin.TextCountEntry
-import info.spiralframework.formats.scripting.lin.TextEntry
 import info.spiralframework.formats.utils.removeEscapes
-import info.spiralframework.base.util.writeInt32LE
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 
 class CustomLin {
     var type = 2
     var header: ByteArray = ByteArray(0)
-    val entries: ArrayList<LinScript> = ArrayList()
+    val entries: MutableList<LinScript> = ArrayList()
+    val linesOfText: MutableList<String> = ArrayList()
+
+    var writeTextBOM: Boolean = true
 
     fun add(entry: LinScript) {
+        if (entry is LinTextScript) addText(entry)
+        entries.add(entry)
+    }
+
+    fun addDirectly(entry: LinScript) {
         entries.add(entry)
     }
 
     fun addAll(entries: Array<LinScript>) {
+        entries.forEach { entry -> if (entry is LinTextScript) addText(entry) }
         this.entries.addAll(entries)
     }
 
     fun addAll(entries: List<LinScript>) {
+        entries.forEach { entry -> if (entry is LinTextScript) addText(entry) }
         this.entries.addAll(entries)
     }
 
-    /**
-     * WARNING: This will not work with UDG!!
-     */
-    fun add(text: String) {
-        entries.add(TextEntry(text, 0))
+    fun addText(textEntry: LinTextScript) {
+        val text = textEntry.text ?: return
+        textEntry.textID = addText(text)
+    }
+    fun addText(text: String): Int {
+        val index = linesOfText.indexOf(text)
+        if (index == -1) {
+            linesOfText.add(text)
+            return linesOfText.size - 1
+        }
+
+        return linesOfText.indexOf(text)
     }
 
     fun compile(out: OutputStream) {
@@ -41,42 +57,54 @@ class CustomLin {
         val textData = ByteArrayOutputStream()
         val textText = ByteArrayOutputStream()
 
-        textData.writeInt32LE(entries.count { entry -> entry is LinTextScript })
-
-        var textID = 0
+        textData.writeInt32LE(linesOfText.size)
 
         if (entries[0] !is TextCountEntry)
-            entries.add(0, TextCountEntry(entries.count { entry -> entry is LinTextScript }))
-        val numText = entries.count { entry -> entry is LinTextScript }
+            entries.add(0, TextCountEntry(linesOfText.size))
 
         entries.forEach { entry ->
             entryData.write(0x70)
             entryData.write(entry.opCode)
 
-            if (entry is LinTextScript) {
-                val strData = (entry.text ?: "Hello, Null!").removeEscapes().toByteArray(Charsets.UTF_16LE)
-                textData.writeInt32LE((numText * 4L) + 8 + textText.size())
-                if(entry.writeBOM) {
-                    textText.write(0xFF)
-                    textText.write(0xFE)
-                }
-
-                textText.write(strData)
-                if(strData[strData.size - 1] != 0x00.toByte() || strData[strData.size - 2] != 0x00.toByte()) {
-                    textText.write(0x00)
-                    textText.write(0x00)
-                }
-
-                entryData.write(textID / 256)
-                entryData.write(textID % 256)
-
-                textID++
-            } else {
+//            if (entry is LinTextScript) {
+//                val strData = (entry.text ?: "Hello, Null!").removeEscapes().toByteArray(Charsets.UTF_16LE)
+//                textData.writeInt32LE((numText * 4L) + 8 + textText.size())
+//                if (entry.writeBOM) {
+//                    textText.write(0xFF)
+//                    textText.write(0xFE)
+//                }
+//
+//                textText.write(strData)
+//                if (strData[strData.size - 1] != 0x00.toByte() || strData[strData.size - 2] != 0x00.toByte()) {
+//                    textText.write(0x00)
+//                    textText.write(0x00)
+//                }
+//
+//                entryData.write(textID / 256)
+//                entryData.write(textID % 256)
+//
+//                textID++
+//            } else {
                 entry.rawArguments.forEach { arg -> entryData.write(arg) }
+//            }
+        }
+
+        linesOfText.forEach { line ->
+            val strData = line.removeEscapes().toByteArray(Charsets.UTF_16LE)
+            textData.writeInt32LE((linesOfText.size * 4L) + 8 + textText.size())
+            if (writeTextBOM) {
+                textText.write(0xFF)
+                textText.write(0xFE)
+            }
+
+            textText.write(strData)
+            if (strData[strData.size - 1] != 0x00.toByte() || strData[strData.size - 2] != 0x00.toByte()) {
+                textText.write(0x00)
+                textText.write(0x00)
             }
         }
 
-        textData.writeInt32LE((numText * 4L) + 8 + textText.size())
+        textData.writeInt32LE((linesOfText.size * 4L) + 8 + textText.size())
 
         if (type == 1)
             out.writeInt32LE(12 + entryData.size() + textData.size() + textText.size())
