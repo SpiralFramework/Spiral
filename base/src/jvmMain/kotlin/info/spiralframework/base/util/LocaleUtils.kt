c4a7c8e4ee049cff6fbe64a38116a2434ef3f5a7
undermybrella <undermybrella@abimon.org>
2019-09-06 18:49:09 +1000

    Delete Stopwatch and UnsignedUtils; swap out SemVer as a type alias for a data class

--- base/src/jvmMain/kotlin/info/spiralframework/base/util/LocaleUtils.kt
+++ base/src/jvmMain/kotlin/info/spiralframework/base/util/LocaleUtils.kt
@@ -1,16 +1,12 @@
 package info.spiralframework.base.util
 
-import info.spiralframework.base.binding.localiseArray
 import info.spiralframework.base.common.SpiralContext
-import info.spiralframework.base.common.locale.SpiralLocale
 
 //TODO: Replace with require
 
 @Deprecated("Use require instead", ReplaceWith("require(statement) { localise(illegalArgument, *illegalParams) }"))
-fun assertAsLocaleArgument(statement: Boolean, illegalArgument: String, vararg illegalParams: Any) {
-    if (!statement)
-        throw IllegalArgumentException(localiseArray(illegalArgument, illegalParams))
-}
+fun assertAsLocaleArgument(statement: Boolean, illegalArgument: String, vararg illegalParams: Any): Nothing =
+        throw IllegalStateException("Localisation requires a context")
 
 /** Prints the given localised [message] to the standard output stream. */
 public inline fun SpiralContext.printLocale(message: String, vararg args: Any) = print(localiseArray(message, args))
@@ -19,11 +15,13 @@ public inline fun SpiralContext.printLocale(message: String, vararg args: Any) =
 public inline fun SpiralContext.printlnLocale(message: String, vararg args: Any) = println(localiseArray(message, args))
 
 /** Prints the given localised [error] to the standard output stream. */
-public inline fun printErrLocale(error: String, vararg args: Any) {
-    System.err.print(localiseArray(error, args))
-}
+@Suppress("DeprecatedCallableAddReplaceWith")
+@Deprecated("Localisation requires a context")
+public inline fun printErrLocale(error: String, vararg args: Any): Nothing =
+        throw IllegalStateException("Localisation requires a context")
 
 /** Prints the given localised [error] and the line separator to the standard output stream. */
-public inline fun printlnErrLocale(error: String, vararg args: Any) {
-    System.err.println(localiseArray(error, args))
-}
+@Suppress("DeprecatedCallableAddReplaceWith")
+@Deprecated("Localisation requires a context")
+public inline fun printlnErrLocale(error: String, vararg args: Any): Nothing =
+        throw IllegalStateException("Localisation requires a context")
