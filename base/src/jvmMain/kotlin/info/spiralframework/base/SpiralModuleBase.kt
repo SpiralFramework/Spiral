11a72cb20f21559c219a2ad3fcf302db3a194c6b
undermybrella <undermybrella@abimon.org>
2019-09-07 11:47:27 +1000

    Add version to modules

--- base/src/jvmMain/kotlin/info/spiralframework/base/SpiralModuleBase.kt
+++ base/src/jvmMain/kotlin/info/spiralframework/base/SpiralModuleBase.kt
@@ -1,5 +1,9 @@
 package info.spiralframework.base
 
+import info.spiralframework.base.common.SPIRAL_VERSION
+import info.spiralframework.base.common.SemanticVersion
+
 class SpiralModuleBase: SpiralModuleProvider {
     override val moduleName: String = "spiral-base"
+    override val moduleVersion: SemanticVersion = SPIRAL_VERSION
 }
--- osl/src/jvmMain/kotlin/info/spiralframework/osl/SpiralModuleOSL.kt
+++ osl/src/jvmMain/kotlin/info/spiralframework/osl/SpiralModuleOSL.kt
@@ -1,7 +1,10 @@
 package info.spiralframework.osl
 
 import info.spiralframework.base.SpiralModuleProvider
+import info.spiralframework.base.common.SPIRAL_VERSION
+import info.spiralframework.base.common.SemanticVersion
 
 class SpiralModuleOSL: SpiralModuleProvider {
     override val moduleName: String = "spiral-osl"
+    override val moduleVersion: SemanticVersion = SPIRAL_VERSION
 }
