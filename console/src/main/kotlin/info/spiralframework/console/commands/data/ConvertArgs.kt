95ddecd9eb8f391c01bbee35d8fb9c97ffb81000
Isaac H <undermybrella@abimon.org>
2019-09-21 18:51:57 +1000

    Fix conversion, and support games being passed in

--- console/src/main/kotlin/info/spiralframework/console/commands/data/ConvertArgs.kt
+++ console/src/main/kotlin/info/spiralframework/console/commands/data/ConvertArgs.kt
@@ -3,24 +3,27 @@ package info.spiralframework.console.commands.data
 import java.io.File
 
 class ConvertArgs {
-    data class Immutable(val converting: File?, var from: String?, var to: String?, var filter: Regex?)
+    data class Immutable(val converting: File?, val from: String?, val to: String?, val filter: Regex?, val game: String?)
 
     var converting: File? = null
     var from: String? = null
     var to: String? = null
     var filter: Regex? = null
     var builder: Boolean = false
+    var game: String? = null
 
     fun makeImmutable(
             defaultConverting: File? = null,
             defaultFrom: String? = null,
             defaultTo: String? = null,
-            defaultFilter: Regex? = null
+            defaultFilter: Regex? = null,
+            defaultGame: String? = null
     ): ConvertArgs.Immutable =
             Immutable(
                     converting ?: defaultConverting,
                     from ?: defaultFrom,
                     to ?: defaultTo,
-                    filter ?: defaultFilter
+                    filter ?: defaultFilter,
+                    game ?: defaultGame
             )
 }
