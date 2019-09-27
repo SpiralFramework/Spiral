21693c8bd662c59d212ccee87b90fccf59464e56
Isaac H <undermybrella@abimon.org>
2019-09-27 22:37:32 +1000

    Fixed merge; added Dr1Anagram

--- console/src/jvmMain/kotlin/info/spiralframework/console/commands/data/ConvertArgs.kt
+++ console/src/jvmMain/kotlin/info/spiralframework/console/commands/data/ConvertArgs.kt
@@ -1,23 +1,24 @@
 package info.spiralframework.console.commands.data
 
+import info.spiralframework.formats.game.DRGame
 import java.io.File
 
 class ConvertArgs {
-    data class Immutable(val converting: File?, val from: String?, val to: String?, val filter: Regex?, val game: String?)
+    data class Immutable(val converting: File?, val from: String?, val to: String?, val filter: Regex?, val game: DRGame?)
 
     var converting: File? = null
     var from: String? = null
     var to: String? = null
     var filter: Regex? = null
     var builder: Boolean = false
-    var game: String? = null
+    var game: DRGame? = null
 
     fun makeImmutable(
             defaultConverting: File? = null,
             defaultFrom: String? = null,
             defaultTo: String? = null,
             defaultFilter: Regex? = null,
-            defaultGame: String? = null
+            defaultGame: DRGame? = null
     ): ConvertArgs.Immutable =
             Immutable(
                     converting ?: defaultConverting,
